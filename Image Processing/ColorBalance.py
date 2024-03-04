0bb2fee73bca2fb217970d041d725b0d2eaf8d0b
ajudamee <clara.raquel711@gmail.com>
2024-03-04 15:23:23 +0100

    Create ColorBalance.py

---
+++ Image Processing/ColorBalance.py
@@ -0,0 +1,27 @@
+#Color balance 
+'''
+Write a simple application to change the color balance of an image
+by multiplying each color value by a different user-specified constant. If you want to get
+fancy, you can make this application interactive, with sliders.
+'''
+
+# 1. Do you get different results if you take out the gamma transformation before or after
+# doing the multiplication? Why or why not?
+
+
+# 2. Take the same picture with your digital camera using different color balance settings
+# (most cameras control the color balance from one of the menus). Can you recover what
+# the color balance ratios are between the different settings? You may need to put your
+# camera on a tripod and align the images manually or automatically to make this work.
+# Alternatively, use a color checker chart (Figure 10.3b), as discussed in Sections 2.3 and
+# 10.1.1.
+
+
+# 3. If you have access to the RAW image for the camera, perform the demosaicing yourself
+# (Section 10.3.1) or downsample the image resolution to get a “true” RGB image. Does
+# 3.9 Exercises 195
+# your camera perform a simple linear mapping between RAW values and the colorbalanced values in a JPEG? Some high-end cameras have a RAW+JPEG mode, which
+# makes this comparison much easier.
+
+
+# 4. Can you think of any reason why you might want to perform a color twist (Section 3.1.2) on the images? See also Exercise 2.9 for some related ideas.
